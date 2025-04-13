24a1ae728e5c9b67416095e17bb97c4834d9ef15
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-04-13 21:47:27 +0100

    updates week eight javascript task

--- week-eight/quiz.js
+++ week-eight/quiz.js
@@ -24,22 +24,25 @@
     // disable(button) should set the button element's attribute "disabled" to the value ""
     // enable(button) should remove the attribute "disabled" from the button element
 
-
-
+    const disable = button => button.setAttribute("disabled", "");
+    const enable = button => button.removeAttribute("disabled");
+    
     // TODO 5: Declare an isCorrect function that compares a guess to the right answer
     // isCorrect(guess) should return true if the guess matches the fact's answer
     
+    const isCorrect = guess => guess === fact.answer;
 
 
     // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
-            // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
+    
+    // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
 
 
-            // TODO 7: Within the event handler function, 
-            // Use a for loop to disable all the option buttons
+    // TODO 7: Within the event handler function, 
+        // Use a for loop to disable all the option buttons
 
 
-            // TODO 8: Within the event handler function,
-            // Get the guessed value from the clicked button
-            // Use a conditional to compare the guess to the fact's answer
-            // and add the "correct"/"incorrect" class as appropriate
+    // TODO 8: Within the event handler function,
+        // Get the guessed value from the clicked button
+        // Use a conditional to compare the guess to the fact's answer
+        // and add the "correct"/"incorrect" class as appropriate
