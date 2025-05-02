b0d4f13a988f642aba3ee499499d6db9ecf95435
mercysunday970 <mirandaeswift686@gmail.com>
2025-05-02 20:00:58 +0100

    Add React Fact

---
+++ First-React/Components/MainContent.jsx
@@ -0,0 +1,14 @@
+export default function MainContent() {
+    return (
+        <main>
+            <h1>Fun facts about React!</h1>
+            <ul className="fact-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Was well over 200k stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>POwers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
