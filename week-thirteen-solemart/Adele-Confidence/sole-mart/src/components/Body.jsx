29c62ec295b359c3104f43e54f0c4961e939caf3
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-05-10 22:09:33 +0100

    restructure foler

---
+++ week-thirteen-solemart/Adele-Confidence/sole-mart/src/components/Body.jsx
@@ -0,0 +1,35 @@
+
+
+const  Body = () => {
+
+    return (
+
+      <>
+      
+
+      <div className=" bg-gray-200 flex flex-row rounded-2xl m-4">
+
+        <div className="grid ml-7">
+          <h1 className="size-40  text-blue-600 text-2xl p-3 font-bold"> Grab upto 5% Off On Selected Shoe</h1>
+          <button className="bg-gray-800 rounded-full text-white text-md mb-30 ml-6 max-w-30"> Buy Now </button>
+        
+        </div>
+
+        
+        
+
+        <div className="flex ml-auto"> 
+        <img src="/image_11.png" alt="Shoe image" />
+        </div>
+      </div>
+
+      </>
+  
+    )
+
+}
+
+
+
+export default Body
+
---
+++ week-thirteen-solemart/Adele-Confidence/sole-mart/src/components/Drop.jsx
@@ -0,0 +1,24 @@
+const Drop = () => {
+    
+           
+        return (
+            <>
+           <div className="flex gap-8 justify-center m-auto mt-3">
+            <button className="bg-gray-300 rounded-full text-black text-md px-4 py-1">Shoe type </button>
+            <button className="bg-gray-300 rounded-full text-black text-md px-4 py-1">Price </button>
+            <button className="bg-gray-300 rounded-full text-black text-md px-4 py-1">Size selection </button>
+            <button className="bg-gray-300 rounded-full text-black text-md px-4 py-1">Color </button>
+            <button className="bg-gray-300 rounded-full text-black text-md px-4 py-1">Material </button>
+            <button className="bg-gray-300 rounded-full text-black text-md px-4 py-1">Brand </button>
+
+            </div>
+
+                 </>
+                
+            
+        )
+    }
+       
+
+export default Drop 
+
